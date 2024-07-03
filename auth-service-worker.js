f60bb013b2affbc1d70812f122831b029e6ae141
jdnbo89 <hello@jayboychuk.com>
2024-07-03 16:02:52 -0700

    Remove auth service worker debug statements

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -9,10 +9,6 @@ self.addEventListener('install', event => {
   // extract firebase config from query string
   const serializedFirebaseConfig = new URL(location).searchParams.get('firebaseConfig');
 
-  console.log("Serialized Firebase Config", serializedFirebaseConfig);
-  console.log("location: ", location);
-  console.log("URL of location: ", new URL(location));
-  
   if (!serializedFirebaseConfig) {
     throw new Error('Firebase Config object not found in service worker query string.');
   }
